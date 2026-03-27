48bdf474841379654a6d8e849d93103e4227389c
Dieudonne Lusolo <mulongeshadieudonne17@gmail.com>
2026-03-27 10:13:15 +0100

    updates

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const form = document.getElementById("mortgage-form");
+const amountInput = document.getElementById("amount");
+const termInput = document.getElementById("term");
+const rateInput = document.getElementById("rate");
+const typeInputs = document.querySelectorAll('input[name="type"]');
+
+const monthlyEl = document.getElementById("monthly");
+const totalEl = document.getElementById("total");
+
+const emptyState = document.getElementById("empty-state");
+const resultsBox = document.getElementById("results");
+
+const clearBtn = document.querySelector(".clear-btn");
+
